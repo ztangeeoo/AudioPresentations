5222f88a940943b5710843f195b0c2d1dcfbc4ab
ztangeeoo <m18218725201@163.com>
2018-03-27 17:48:50 +0800

    first

---
+++ src/main/java/list/controller/ListController.java
@@ -0,0 +1,53 @@
+package list.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author ztang
+ * @date 11:03 2018/3/27
+ */
+@RestController
+public class ListController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ListController.class);
+
+    // 单文件上传
+    @PostMapping(value = "/upload")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                return "文件为空";
+            }
+            // 获取文件名
+            String fileName = file.getOriginalFilename();
+            logger.info("上传的文件名为：" + fileName);
+            // 获取文件的后缀名
+            String suffixName = fileName.substring(fileName.lastIndexOf("."));
+            logger.info("文件的后缀名为：" + suffixName);
+
+            // 设置文件存储路径
+            String filePath = "D://aim//";
+            String path = filePath + fileName + suffixName;
+
+            File dest = new File(path);
+            // 检测是否存在目录
+            if (!dest.getParentFile().exists()) {
+                dest.getParentFile().mkdirs();// 新建文件夹
+            }
+            file.transferTo(dest);// 文件写入
+            return "上传成功";
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+}
---
+++ src/main/java/list/controller/TestController.java
@@ -0,0 +1,17 @@
+package list.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ztang
+ * @date 10:46 2018/3/27
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        return "helloworld";
+    }
+}
